6e36938db69c89932e1467462b08056c54df0054
jcdsaez <jcdsaez@gmail.com>
2022-07-31 23:55:10 -0300

    agregue unos archivos y arregle unas cosas del login, sigue en revisión

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-document.getElementById(destacado)
-onchange
-onclick
-onmouseover
-    /*si se pasa el mouse sobre el icono de edicion que se haga mas grande
-    */
-onkeydown
+var btn = document.getElementById('ingresar');
+var clave = document.getElementById('clave');
+var usuario = document.getElementById('usuario');
+
+
+
+btn.addEventListener('click', function(evt){
+
+      if(clave.value === ''){
+
+          console.log('el campo contraseña es obligatorio')
+          evt.preventDefault();
+          return false;
+
+      }else if(usuario.value === ''){
+
+      console.log('el campo de usuario es obligatorio')
+          evt.preventDefault();
+          return false;
+
+      }else if(usuario.value.length > 30){
+
+        console.log('El nombre del usuario es demasiado largo')
+          evt.preventDefault();
+          return false;
+
+      }
+
+
+
+
+});
